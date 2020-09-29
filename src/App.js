7490ae8dd7e925c3b1933a93d61d999e99d2f727
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2020-09-29 16:15:47 -0700

    finished practice problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Profile from './Profile'
+import AppContext from './AppContext'
 
 class App extends React.Component {
 
@@ -22,13 +24,44 @@ class App extends React.Component {
       ]
     }
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let firstName = event.target.firstName.value;
+    let lastName = event.target.lastName.value;
+    let role = event.target.role.value;
+    let idd = event.target.idd.value
+    let allProfiles = {firstName: firstName, lastName: lastName, role: role, id: idd}
   
-  render(){
+    this.setState({
+      profiles: [...this.state.profiles, allProfiles]
+    })
+  }
+  
+  render() {
+    let contextValues = {
+      profiles: this.state.profiles
+    }
     return (
+      <AppContext.Provider value={contextValues}>
       <div>
+      <h1>Profiles</h1>
           {/* Display the full list of Profiles */}
           {/* Add a form to add a new profile to the list  */}
+          <form onSubmit={(event) => {this.handleSubmit(event)}}>
+            <label htmlFor="firstName">First Name:</label>
+            <input type="textarea" id="firstName" name="firstName"/>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="textarea" id="lastName" name="lastName"/>
+            <label htmlFor="role">Role:</label>
+            <input type="textarea" id="role" name="role"/>
+            <label htmlFor="id">ID: </label>
+            <input type="textarea" id="idd" name="id"/>
+            <button type="submit">Submit</button>
+          </form>
+          <Profile profile={this.state.profiles} />
       </div>
+      </AppContext.Provider>
     );
   }
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,1 +1,25 @@
+import React from 'react'
+import AppContext from './AppContext'
 
+class Profile extends React.Component {
+
+    static contextType = AppContext;
+
+    render() {
+       
+        return(
+           <ul>
+                {this.context.profiles.map((profile, index) => {
+                   return <li key={index}>
+                       Name: {profile.firstName} {profile.lastName} <br/>
+                       Role: {profile.role} <br/>
+                       ID: {profile.id} </li>
+                })}
+            </ul>
+          
+        )
+    }
+}
+
+
+export default Profile
